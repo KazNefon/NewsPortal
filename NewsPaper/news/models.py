71402b161c3547e1d596bf63ad5c2c110a0b3a1e
KazNefon <154060060+KazNefon@users.noreply.github.com>
2024-03-21 20:23:39 +0200

    Add files via upload

---
+++ NewsPaper/news/models.py
@@ -0,0 +1,73 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        self.rating = 3 * sum(self.post_set.all().values_list('rating', flat=True))
+        self.rating += sum(self.comment_set.all().values_list('rating', flat=True))
+        self.rating += sum(self.post_set.all().values_list('comment__rating', flat=True))
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    TYPE_CHOICES = [
+        ('article', 'Статья'),
+        ('news', 'Новость'),
+    ]
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    created_date = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.content[:124] + '...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+from django.db import models
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
